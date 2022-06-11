f9e898d7bcac2c9f7490ae34a72127abd4eb1d05
AbdulHakim201103 <abdulhakim201103@gmail.com>
2022-06-11 13:50:19 +0600

    lest

--- src/Pagas/Home/Home/Details.js
+++ src/Pagas/Home/Home/Details.js
@@ -37,9 +37,9 @@ const Details = () => {
   return (
     <div>
       <div className="container mx-auto">
-        <h1 className="text-center text-primary text-5xl mt-20">Purchase</h1>
+        <h1 className="text-center text-primary text-5xl mt-20">Details</h1>
         <div>
-          <div className="hero my-40">
+          <div className="hero my-10">
             <div className="hero-content flex-col lg:flex-row">
               <div className="h-96 carousel carousel-vertical rounded-box">
                 <div className="carousel-item h-full">
@@ -53,10 +53,10 @@ const Details = () => {
                 </div>
               </div>
               <div className="mx-5">
-                <h2 className="text-4xl text-primary mx-auto py-3">{name}</h2>
-                <h2 className="text-xl text-primary mx-auto py-3">Status:{status}</h2>
+                <h2 className="text-5xl text-primary mx-auto py-3">{name}</h2>
+                <h2 className="text-xl  mx-auto py-3"><span className="text-secondary text-xl font-bold">Status:</span> {status}</h2>
                 <h2 className="text-xs mx-auto py-3">Date: {date}</h2>
-                <h2 className="text-xl font-bold mx-auto py-3">Desrption:</h2>
+                <h2 className="text-xl font-bold mx-auto py-3 text-secondary">Desrption:</h2>
                 <p className="my-2 px-3"> ● {desc1}</p>
                 <p className="my-2 px-3"> ● {desc2}</p>
                 <p className="my-2 px-3"> ● {desc3}</p>
@@ -69,7 +69,7 @@ const Details = () => {
                 <p className="my-2 px-3"> ● {desc10}</p>
                 <p className="my-2 px-3"> ● {desc11}</p>
                 <p className="my-2">
-                  <spen className="text-xl font-bold">Tools:</spen> {tools}
+                  <spen className="text-xl font-bold text-secondary">Tools:</spen> {tools}
                 </p>
                 <div className="my-4">
                   <a target="blank" className="btn btn-primary mx-2" href={live}>
